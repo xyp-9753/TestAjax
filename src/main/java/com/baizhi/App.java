3cdfbe5d0cee473684326a1f67ab72aacc5141d7
Xuyp <xuyanpengdidi@163.com>
2019-10-14 17:06:40 +0800

    第一次提交 v1.0

--- src/main/java/com/baizhi/App.java
+++ src/main/java/com/baizhi/App.java
@@ -11,5 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
     }
 }
